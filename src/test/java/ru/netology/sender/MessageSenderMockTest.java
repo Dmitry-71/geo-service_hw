95572d804204e47eec64662404e5c66d5f918256
Dmitry_71 <dgorodilov@mail.ru>
2024-03-05 14:14:55 +0300

    initial commit

---
+++ src/test/java/ru/netology/sender/MessageSenderMockTest.java
@@ -0,0 +1,46 @@
+package ru.netology.sender;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.netology.GeoService;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.i18n.LocalizationService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ExtendWith(MockitoExtension.class)
+public class MessageSenderMockTest {
+    @Mock
+    public GeoService geoService;
+    @Mock
+    public LocalizationService localizationService;
+    @InjectMocks
+    public MessageSenderImpl messageSender;
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "127.0.0.1,     LOCALHOST,  'Hello world!'",
+            "172.0.32.11б,  RUSSIA,     'Добро пожаловать'",
+            "172.0.0.1,     RUSSIA,     'Добро пожаловать'",
+            "96.44.183.149, USA,        'Welcome'",
+            "96.0.0.1,      USA,        'Welcome'"
+    })
+    public void testSend(String ip, String country, String excepted) {
+        Country countryEnum = Country.valueOf(country);
+        Mockito.when(geoService.byIp(ip)).thenReturn(new Location(null, countryEnum, null, 0));
+        Mockito.when(localizationService.locale(countryEnum)).thenReturn(excepted);
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, ip);
+        String actual = messageSender.send(headers);
+        Assertions.assertEquals(excepted, actual);
+    }
+}
